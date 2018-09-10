f4802739477c39600fc3e563a8cc466c26bba495
Gabriel Dahia <gdahia@gmail.com>
2018-09-09 23:21:18 -0300

    Add 02-H solution

--- 02/spoj-LCS0.cpp
+++ 02/spoj-LCS0.cpp
@@ -1,45 +1,50 @@
-#include <algorithm>
-#include <iostream>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
+char a[50001];
+char b[50001];
+unsigned long long pm[26][50001];
+unsigned long long v[50001];
+
 int main() {
-  string a, b;
-  cin >> a >> b;
-  const int n = a.size();
-  const int m = b.size();
-
-  vector<int> a_char_pos[26];
-  for (int i = 0; i < n; i++) a_char_pos[a[i] - 'a'].push_back(i);
-  for (int i = 0; i < 26; i++)
-    reverse(a_char_pos[i].begin(), a_char_pos[i].end());
-
-  vector<int> seq;
-  for (int i = 0; i < m; i++) {
-    const char c = b[i];
-    for (const int index : a_char_pos[c - 'a']) seq.push_back(index);
+  scanf("%s%s", a, b);
+  int n = strlen(a);
+  int m = strlen(b);
+  if (n < m) {
+    swap(a, b);
+    swap(n, m);
   }
+  const int sz = (n + 63) / 64;
 
-  const int len = seq.size();
-  vector<int> aux;
-  aux.push_back(-1);
-  for (int i = 0; i < len; i++) {
-    int lis = 1;
-    int l = 1, r = aux.size() - 1;
-    while (l <= r) {
-      int m = (l + r) / 2;
-      if (aux[m] <= seq[i])
-        l = m + 1;
-      else
-        r = m - 1;
+  unsigned long long bitpos = 1;
+  int vetpos = 0;
+  for (int i = 0; i < n; i++) {
+    pm[a[i] - 'a'][vetpos] |= bitpos;
+    bitpos <<= 1;
+    if (!bitpos) {
+      bitpos = 1;
+      vetpos++;
     }
-    lis = l;
-    if (lis == aux.size())
-      aux.push_back(seq[i]);
-    else
-      aux[lis] = min(aux[lis], seq[i]);
   }
 
-  cout << aux.size() - 1 << endl;
+  for (int j = 0; j < m; j++) {
+    const char c = b[j] - 'a';
+    unsigned long long carry = 1;
+    for (int i = 0; i < sz; i++) {
+      const unsigned long long u = (v[i] | pm[c][i]);
+      const unsigned long long tmp_carry = (v[i] >> 63);
+      const unsigned long long mask = (u - ((v[i] << 1) | carry));
+      v[i] = (u & (~mask));
+      carry = (mask > u ? 1 : 0) + tmp_carry;
+    }
+  }
+
+  int ans = 0;
+  for (int i = 0; i < sz; i++)
+    while (v[i]) {
+      v[i] &= v[i] - 1;
+      ans++;
+    }
+
+  printf("%d\n", ans);
 }
